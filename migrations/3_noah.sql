5193e8dd60917e0ce4ad1f791a633a0666b3ee05
Philippe Voinov <philippevoinov@gmail.com>
2018-08-15 21:18:03 +0200

    Get it right.

--- migrations/3_noah.sql
+++ migrations/3_noah.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 INSERT INTO users (username, password) VALUES
-  ('noah', '$2a$10$120SCt02YaOiJadGwgKYTe9e/HDmu6JXjtRQNyTx683kvHoT0kc0e'),
+  ('noah', '$2a$10$120SCt02YaOiJadGwgKYTe9e/HDmu6JXjtRQNyTx683kvHoT0kc0e');
 
 -- +migrate Down
 DELETE FROM users WHERE username = 'noah';
