52db8fabc58eea5ac069c2dd464ff47921fc0b98
Philippe Voinov <philippevoinov@gmail.com>
2018-08-29 12:08:16 +0200

    Fix cup alignment.

--- frontend/src/components/cup.tsx
+++ frontend/src/components/cup.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { colors } from "../style";
 import Ratio from "./ratio";
+import { css } from "glamor";
 
 interface Props {
   width: string;
@@ -9,16 +10,23 @@ interface Props {
 
 const HEIGHT = 0.65;
 
+const styles = {
+  svg: css({
+    display: "block",
+    overflow: "visible",
+  }),
+};
+
 export default class Cup extends React.PureComponent<Props> {
   render() {
     const { width, center } = this.props;
     return (
       <Ratio ratio={HEIGHT} width={width}>
         <svg
-          viewBox={`${center ? "0.5" : "0"},${1 - HEIGHT},1,${HEIGHT}`}
+          viewBox={`${center ? "0.5" : "0"},0,1,${HEIGHT}`}
           width="100%"
           height="100%"
-          style={{ overflow: "visible" }}
+          {...styles.svg}
         >
           <polygon
             points={`0,0 1,0 0.85,${HEIGHT} 0.15,${HEIGHT}`}
